4ec42d8f992e992de8f28032991b8d96e56c6442
Бродников Денис <platz198@yandex.ru>
2022-03-21 08:39:12 +0700

    modification

--- src/Client.java
+++ src/Client.java
@@ -6,17 +6,18 @@ import java.net.Socket;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String host = "127.0.0.1";
         int port = 8089;
-        Socket clientSocket = new Socket(host, port);
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out.println("Host: " + host);
-
-        String resp = in.readLine();
-        System.out.println(resp);
-        clientSocket.close();
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Host: " + host);
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
